3722a764809f2a4a33fb7ccc61af7efea74f78a9
Arpine Oganisian <arpineoganisian@gmail.com>
2022-06-06 23:26:41 +0300

    p1 installing server

--- README.md
+++ README.md
@@ -12,6 +12,10 @@ _System Administration related exercise_
 
 `vagrant status` tells the state of the machines
 
+`vagrant ssh [name]` will SSH into a machine and give you access to a shell
+
+
+
 ### p2: K3s and three simple applications
 
 ### p3: K3d and Argo CD
@@ -24,9 +28,10 @@ _System Administration related exercise_
 - [Vagrant Boxes](https://app.vagrantup.com/boxes/search)
 - [Vagrant Documentation](https://www.vagrantup.com/docs)
 - [Vagrantfile Documentation](vagrantup.com/docs/vagrantfile)
-- [VBoxManage - утилита для внесения изменений в виртуальную машину из командной строки](https://www.virtualbox.org/manual/ch08.html)
+- [VBoxManage - утилита для внесения изменений в виртуальную машину из командной строки](https://docs.oracle.com/en/virtualization/virtualbox/6.0/user/vboxmanage-modifyvm.html)
 - [Про мини-кластеры в Kubernetes в целом](https://habr.com/ru/company/flant/blog/572188/)
 - [K3s](https://k3s.io/)
+- [K3s documentation](https://rancher.com/docs/k3s/latest/en/)
 - [K3d](https://k3d.io/v5.4.2/)
 - [Как исправить ошибку с диапазоном IP-адресов](https://www.virtualbox.org/manual/ch06.html#network_hostonly)
 
--- p1/install_server.sh
+++ p1/install_server.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+# CentOS Linux 8 had reached the End Of Life (EOL) on December 31st, 2021.
+# It means that CentOS 8 will no longer receive development resources from the official CentOS project.
+# After Dec 31st, 2021, if you need to update your CentOS, you need to change the mirrors to vault.centos.org where they will be archived permanently.
+cd /etc/yum.repos.d/
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+sudo yum update -y
+
+cd
+
+
+# Version of K3s to download from Github. Will attempt to download from the stable channel if not specified
+export INSTALL_K3S_VERSION=v1.23.6+k3s1
+# K3S_KUBECONFIG_MODE - The option to allow writing to the kubeconfig file is useful for allowing a K3s cluster to be imported into Rancher
+export K3S_KUBECONFIG_MODE="644"
+# Command with flags to use for launching K3s in the service.
+# If the command is not specified, and the K3S_URL is set, it will default to “agent.”
+# If K3S_URL not set, it will default to “server.”
+export INSTALL_K3S_EXEC="--node-ip 192.168.42.110"
+
+# Installation with Script
+curl -sfL https://get.k3s.io | sh -
